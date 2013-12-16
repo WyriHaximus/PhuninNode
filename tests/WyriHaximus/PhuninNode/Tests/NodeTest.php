85dd2efa4c9a4b0556593c41236031a17a16d7be
Cees-Jan <ceesjank@gmail.com>
2013-12-17 00:35:22 +0100

    Rewritten all the test for Node

--- tests/WyriHaximus/PhuninNode/Tests/NodeTest.php
+++ tests/WyriHaximus/PhuninNode/Tests/NodeTest.php
@@ -11,20 +11,135 @@
 
 namespace WyriHaximus\PhuninNode\Tests;
 
-class NodeTest extends AbstractConnectionContextTest {
-    
-    public function testIsUp() {
-        $i = 0;
-        $this->conn->on('data', function ($data) use (&$i) {
-            switch ($i) {
-                case 0:
-                    $this->assertEquals("# munin node at HOSTNAME\n", $data);
-                    $this->conn->write('quit' . PHP_EOL);
-                    break;
-            }
-            $i++;
-        });
-        $this->loop->run();
-    }
+class NodeTest extends \PHPUnit_Framework_TestCase {
+	public function setUp() {
+		$this->loop = $this->getMock('\React\EventLoop\StreamSelectLoop');
+		$this->socket = $this->getMock('\React\Socket\Server', [
+			'on',
+			'write',
+			'shutdown',
+		], [
+			$this->loop,
+			'0.0.0.0',
+			12345
+		]);
+
+		$this->plugins = new \SplObjectStorage();
+		$plugin = $this->getMock('\WyriHaximus\PhuninNode\Interfaces\Plugin', [
+			'getSlug',
+			'getConfiguration',
+			'setNode',
+			'getValues',
+		]);
+		$plugin->method('getSlug')
+			->willReturn('a');
+		$plugin->method('getConfiguration')
+			->will($this->returnCallback(function($resolver) {
+				$configuration = new \WyriHaximus\PhuninNode\PluginConfiguration();
+				$configuration->setPair('graph_category', 'a');
+				$resolver->resolve($configuration);
+			}));
+		$this->plugins->attach($plugin);
+	}
+
+	public function tearDown() {
+		unset($this->loop, $this->socket, $this->plugins);
+	}
+
+	public function testConstruct() {
+		$this->socket->expects($this->once())
+			->method('on')
+			->with('connection', $this->callback(function($callback) {
+				return $this->isInstanceOf('\WyriHaximus\PhuninNode\Node', $callback[0]) && $this->identicalTo('onConnection', $callback[1]);
+			}));
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+	}
+
+	public function testShutdown() {
+		$this->socket->expects($this->once())
+			->method('shutdown');
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$node->shutdown();
+	}
+
+	public function testOnConnection() {
+		$connection = $this->getMock('\React\Socket\Connection', [], [
+			$this->socket,
+			$this->loop,
+		]);
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$node->onConnection($connection);
+		$connections = $node->getConnections();
+		$connections->rewind();
+		$this->assertSameSize(['a'], $connections);
+		$this->assertInstanceOf('\WyriHaximus\PhuninNode\ConnectionContext', $connections->current());
+	}
+
+	public function testOnClose() {
+		$connection = $this->getMock('\React\Socket\Connection', [], [
+			$this->socket,
+			$this->loop,
+		]);
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$node->onConnection($connection);
+		$connections = $node->getConnections();
+		$connections->rewind();
+		$node->onClose($connections->current());
+		$this->assertSameSize([], $node->getConnections());
+	}
+
+	public function testAddPlugin() {
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$this->assertSameSize([], $node->getPlugins());
+		$this->plugins->rewind();
+		$node->addPlugin($this->plugins->current());
+		$this->assertSameSize(['a'], $node->getPlugins());
+	}
+
+	public function testGetPluginsEmpty() {
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$this->assertSameSize([], $node->getPlugins());
+	}
+
+	public function testGetPluginsOne() {
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$this->plugins->rewind();
+		$node->addPlugin($this->plugins->current());
+		$this->assertSameSize(['a'], $node->getPlugins());
+	}
+
+	public function testGetConnectionsEmpty() {
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$this->assertSameSize([], $node->getConnections());
+	}
+
+	public function testGetConnectionsOne() {
+		$connection = $this->getMock('\React\Socket\Connection', [], [
+			$this->socket,
+			$this->loop,
+		]);
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$node->onConnection($connection);
+		$this->assertSameSize(['a'], $node->getConnections());
+	}
+
+	public function testGetPlugin() {
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$this->plugins->rewind();
+		$node->addPlugin($this->plugins->current());
+		$this->assertSame($this->plugins->current(), $node->getPlugin('a'));
+		$this->assertTrue(!$node->getPlugin('b'));
+	}
+
+	public function testResolverFactory() {
+		$called = false;
+		$node = new \WyriHaximus\PhuninNode\Node($this->loop, $this->socket);
+		$resolver = $node->resolverFactory(function() use (&$called) {
+			$called = true;
+		});
+		$resolver->resolve();
+
+		$this->assertTrue($called);
+	}
     
 }
