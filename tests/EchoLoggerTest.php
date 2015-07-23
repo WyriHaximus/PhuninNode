7282b81c32db259d9a4d281a224332f4d0e38e84
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-07-23 07:44:13 +0200

    Test for the echo logger

---
+++ tests/EchoLoggerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of PhuninNode.
+ *
+ ** (c) 2015 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WyriHaximus\PhuninNode\Tests;
+
+use WyriHaximus\PhuninNode\EchoLogger;
+
+/**
+ * Class EchoLoggerTest
+ * @package WyriHaximus\PhuninNode
+ */
+class EchoLoggerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLog()
+    {
+        $logger = new EchoLogger();
+        ob_start();
+        $logger->log('abc', 'def');
+        $this->assertSame('[abc] def' . PHP_EOL, ob_get_clean());
+    }
+}
