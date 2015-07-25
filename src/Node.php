8b02eaa5f6d7fc63b3333b23d238927a22b54510
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-07-25 16:06:59 +0200

    File header update and import ordering

--- src/Node.php
+++ src/Node.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of PhuninNode.
  *
- ** (c) 2013 - 2014 Cees-Jan Kiewiet
+ ** (c) 2013 - 2015 Cees-Jan Kiewiet
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
